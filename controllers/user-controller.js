887b3c43c1c5b1902a4990e7bfcf207c0c265b8a
Max Kohn <mkohn@mkohn-ltm4.internal.salesforce.com>
2022-02-05 11:29:36 -0500

    added all user routes and verified

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -28,6 +28,26 @@ const userController = {
         User.create(body)
             .then(dbUserData => res.json(dbUserData))
             .catch(err => res.status(400).json(err));
+    },
+    updateUser({params, body}, res) {
+        User.findOneAndUpdate({_id: params.id}, body, {new:true, runValidators: true})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'no user found with thiis id'});
+                    return;
+                }
+                res.json(dbUserData);
+            }).catch(err => console.log(err));
+    },
+    deleteUser({params}, res) {
+        User.findOneAndDelete({_id: params.id})
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({message: 'no user with this id found'});
+                    return;
+                }
+                res.json(dbUserData);
+            }).catch(err => console.log(err));
     }
 }
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -5,7 +5,10 @@ router.route('/')
     .get(userController.getAllUsers)
     .post(userController.createUser);
 
-    
-router.route('/:id').get(userController.getUserById);
+
+router.route('/:id')
+    .get(userController.getUserById)
+    .put(userController.updateUser)
+    .delete(userController.deleteUser)
 
 module.exports = router;
